4bed67cd8bbe895b9232086496ca061961027e51
temp <misc@t3mp.co.uk>
2026-08-05 02:32:09 +0100

    backup (26w32b)

--- scripts/the-end.js
+++
@@ -1,7 +0,0 @@
-import { ACH, achGoal, achValue, ready, textbox } from "./textbox.js";
-
-const END = "free-the-end";
-
-ready.then(function () {
-  textbox(achValue(END) >= achGoal(ACH[END]) ? "ending" : "ending-locked");
-});
